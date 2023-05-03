38d1af3a443f5e8930b6b173b51b662ee452ce82
Kautilya Save <kautilyasave@gmail.com>
2023-05-02 21:39:08 -0600

    Storyboard UI and basic View controller setup

--- YetAnotherWeatherApp/YetAnotherWeatherApp/ViewController.swift
+++ YetAnotherWeatherApp/YetAnotherWeatherApp/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    
+    // IBOutlets
+    @IBOutlet weak var weatherSearchField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var shareLocation: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+    
     }
 
 
